a022c1cae000c9c876e951083b8988669ae9885f
l <henry.kirya@gmail.com>
2019-06-30 22:07:37 +0300

    nuff for tonight

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,34 @@
-#write your code here
+def translate(ww)
+  mega = ''
+  aa = ww.split
+  aa.each do |e|
+    tt = trans(e)
+    if aa.length >1 and e != aa.last
+      tt += ' '
+    end  
+    mega += tt
+  end
+  mega
+end
+
+
+def trans(ww) 
+  vowels = ['a','e','i','o','u']
+  if vowels.include?(ww[0])
+    ss = ww+'ay'
+  elsif vowels.include?(ww[1])
+    ss = ww[1,ww.length]
+    ss = ss+ww[0]+'ay'
+  elsif vowels.include?(ww[2])
+    ss = ww[2,ww.length]
+    ss = ss+ww[0]+ww[1]+'ay'
+  elsif vowels.include?(ww[3])
+    ss = ww[3,ww.length]
+    ss = ss+ww[0]+ww[1]+ww[2]+'ay'
+  end
+  ss
+end
+
+
+
+
