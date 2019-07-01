c503f2f420b3096abe0601e5d405c4833eeb76cd
l <henry.kirya@gmail.com>
2019-07-01 15:48:17 +0300

    added book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
-# write your code here
+  attr_writer:title
+
+  def title
+    words = @title.split
+    bb = []
+    little = ['and','the','a','an','in','of']
+    words.each do |w|
+      if little.include? w
+        nw = w
+      else
+        nw = w.capitalize
+      end
+      bb << nw
+    end
+    if little.include? bb.first
+      bb[0] = bb.first.capitalize
+    end
+    bb.join(' ')
+  end
+
 end
